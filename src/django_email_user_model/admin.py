9ee81e1e784f2ea1a93dc4b8563d62c7a5e9227f
Jeff Buttars <jeffbuttars@gmail.com>
2015-09-14 18:08:59 -0700

    Fix some imports

--- src/django_email_user_model/admin.py
+++ src/django_email_user_model/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import forms
 
 from .models import EmailUserModel
 
 # We need to override the base User creation and change forms
 # because they are hard coded to use the User model, we just
 # need to make them use our EmailUserModel model
+
 
 # Override the forms:
 class EmailUserCreationForm(UserCreationForm):
@@ -23,6 +25,7 @@ class EmailUserCreationForm(UserCreationForm):
             self._meta.model._default_manager.get(username=username)
         except self._meta.model.DoesNotExist:
             return username
+
         raise forms.ValidationError(self.error_messages['duplicate_username'])
 
 
